65b6e906711f8c8e0f08b45f598a01383c659dec
cine7 <contatose7ima@gmail.com>
2016-12-08 13:29:18 -0300

    teste 02 - 08.12

--- projeto/ProjetoGrupo6/DAL/DALAvaliacaoComentario.cs
+++ projeto/ProjetoGrupo6/DAL/DALAvaliacaoComentario.cs
@@ -59,30 +59,21 @@ namespace ProjetoGrupo6.DAL
         public bool SelectValidarAvaliacaoComentario(int comentario_id, string usuarioAvaliador)
         {
             bool validar = false;
-            // Cria Conexão com banco de dados
             SqlConnection conn = new SqlConnection(connectionString);
-            // Abre conexão com o banco de dados
             conn.Open();
-            // Cria comando SQL
             SqlCommand cmd = conn.CreateCommand();
-            // define SQL do comando
             cmd.CommandText = "SELECT dataHora FROM AvaliacaoComentario where comentario_id = @comentario_id and usuarioAvaliador = @usuarioAvaliador";
             cmd.Parameters.AddWithValue("@comentario_id", comentario_id);
             cmd.Parameters.AddWithValue("@usuarioAvaliador", usuarioAvaliador);
-            // Executa comando, gerando objeto DbDataReader
             SqlDataReader dr = cmd.ExecuteReader();
-            // Le titulo do livro do resultado e apresenta no segundo rótulo
             if (dr.HasRows)
             {
-                while (dr.Read()) // Le o proximo registro
+                while (dr.Read()) 
                 {
                     if (dr["dataHora"] != null) validar = true;
-                    // Adiciona o livro lido à lista
                 }
             }
-            // Fecha DataReader
             dr.Close();
-            // Fecha Conexão
             conn.Close();
             return validar;
         }
